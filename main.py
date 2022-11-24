c55d246ccb64950ba2f75eb4732c9b9f6c9dd5c6
noamephrat94 <77915253+noamephrat94@users.noreply.github.com>
2022-11-24 15:15:20 +0200

    Changed driver settings

--- main.py
+++ main.py
@@ -25,16 +25,12 @@ month = datetime.today().strftime('%m')
 year = datetime.today().strftime('%Y')
 
 def prep_driver():
-    driver_location = 'driver/chromedriver'
-    options = webdriver.ChromeOptions()
-    options.add_argument('--lang=es')
-    prefs = {
-        "translate_whitelists": {"hr": "en"},
-        "translate": {"enabled": "true"}
-    }
-    options.add_experimental_option("prefs", prefs)
-    # options.add_argument("--headless")
-    driver = webdriver.Chrome(executable_path=driver_location, chrome_options=options)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+#     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     return driver
 
 
